dc9be73c93aef277181f014cc8cf5bc0978a2e5b
Henrik Jonsson <me@hkjn.me>
2018-01-20 17:04:12 +0000

    bitcoin: Add entrypoint.sh

---
+++ bitcoin/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -euo pipefail
+
+declare IP_ADDR=${IP_ADDR:-""}
+declare LOG_LEVEL=${LOG_LEVEL:-"info"}
+declare ALIAS=${ALIAS:-"hkjn"}
+
+if [ ! "${IP_ADDR}" ]; then
+	echo "FATAL: No IP_ADDR specified." >&2
+fi
+
+echo "Starting bitcoind.."
+bitcoind -dbcache=800 -onlynet=ipv4 &
+
+echo "Starting lightningd.."
+lightningd --network=bitcoin --ipaddr=${IP_ADDR} --log-level=${LOG_LEVEL} --alias=${ALIAS}
