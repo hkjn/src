794414649d6ec7817ee7abb6496d82b545a68349
Henrik Jonsson <me@hkjn.me>
2018-01-20 10:21:43 +0000

    bitcoin: Add connect_ln.sh script

---
+++ bitcoin/connect_ln.sh
@@ -0,0 +1,6 @@
+#
+# Connect to known LN peers on mainnet.
+#
+set -euo pipefail
+lightning-cli connect 03d04b48cb2f277055f765c330de3c3c84f4e7d72129624bdb9b272d1113f13f14 176.10.136.25
+lightning-cli connect 03939ff69d65a13c4bb2585042e7eb7e75a7c77289ab5794d1b973721d86c6839c 213.113.59.152
