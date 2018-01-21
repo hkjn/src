ef86317d6155a551ee33815e0822b18429c0206c
Henrik Jonsson <me@hkjn.me>
2018-01-21 19:10:27 +0000

    bitcoin: Add more LN mainnet nodes

--- bitcoin/connect_mainnet_ln.sh
+++ bitcoin/connect_mainnet_ln.sh
@@ -14,3 +14,7 @@ lightning-cli connect 02f6725f9c1c40333b67faea92fd211c183050f28df32cac3f9d69685f
 lightning-cli connect 03b7ca940bc33b882dc1f1bee353a6cf205b1a7472d8ae24d45370a8f510c27d23 18.195.40.124
 # #RECKLESS
 lightning-cli connect 035f1498c929d4cefba4701ae36a554691f526ff60b1766badd5a49b3c8b68e1d8 78.63.23.25
+# UNITEDBOUNCE
+lightning-cli connect 029113620f929df927a4877ae6727b214418c366bf09ebbd4552eda235f48c00f5 68.129.210.251
+# HectorJ from reddit
+lightning-cli connect 0207481a19a3f51a48f134e95afa67cfeffdb38a99b5ad3494a320c4918aaaf579 163.172.174.151
