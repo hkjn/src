c9afc6e5f1497ff0be5a54cd5d0ff4e894f46335
Henrik Jonsson <me@hkjn.me>
2018-01-21 19:20:01 +0000

    bitcoin: Tweak lightningd flags

--- bitcoin/entrypoint.sh
+++ bitcoin/entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 declare IP_ADDR=${IP_ADDR:-""}
 declare LOG_LEVEL=${LOG_LEVEL:-"info"}
-declare ALIAS=${ALIAS:-"hkjn"}
+declare ALIAS=${ALIAS:-"ln.hkjn.me"}
 
 if [ ! "${IP_ADDR}" ]; then
 	echo "FATAL: No IP_ADDR specified." >&2
@@ -14,4 +14,4 @@ echo "Starting bitcoind.."
 bitcoind -dbcache=800 -onlynet=ipv4 &
 
 echo "Starting lightningd.."
-lightningd --network=bitcoin --ipaddr=${IP_ADDR} --log-level=${LOG_LEVEL} --alias=${ALIAS}
+lightningd --network=bitcoin --ipaddr=${IP_ADDR} --log-level=${LOG_LEVEL} --alias=${ALIAS} --rgb=003366
