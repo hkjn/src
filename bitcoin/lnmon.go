ea1164532402d3e0e1f8919de992bcaceb36285e
Henrik Jonsson <me@hkjn.me>
2018-01-24 15:06:38 +0000

    lnmon: Add bitcoind check

--- bitcoin/lnmon.go
+++ bitcoin/lnmon.go
@@ -4,20 +4,32 @@ package main
 import (
 	"bytes"
 	"log"
-	"os"
 	"os/exec"
+	"strings"
 )
 
-type cli struct{}
+type (
+	cli          struct{}
+	bitcoindInfo struct {
+		pid  int
+		args []string
+	}
+)
 
-func (c cli) exec(cmd string) (string, error) {
-	cmd := exec.Command("lightning-cli", cmd)
+var btcInfo bitcoindInfo
+
+func execCmd(cmd string, arg ...string) (string, error) {
+	c := exec.Command(cmd, arg...)
 	out := bytes.Buffer{}
-	cmd.Stdout = &out
-	if err := cmd.Run(); err != nil {
+	c.Stdout = &out
+	if err := c.Run(); err != nil {
 		return "", err
 	}
 	return out.String(), nil
+}
+
+func (c cli) exec(cmd string) (string, error) {
+	return execCmd("lightning-cli", cmd)
 }
 
 func (c cli) GetInfo() (string, error) {
@@ -25,21 +37,38 @@ func (c cli) GetInfo() (string, error) {
 }
 
 func (c cli) GetPeers() (string, error) {
+	return "", nil
 }
 
 func (c cli) GetNodes() (string, error) {
+	return "", nil
 }
 
-//{ "id" : "02501223d013c9c8da04fa7d482570cb38416f6faade7bd86f03413915557e0ff7", "port" : 19735, "address" :
-//       [   { "type" : "ipv4", "address" : "163.172.162.18", "port" : 9735 } ], "version" : "v0.5.2-2016-11-21-1599-gf298c6b0", "blockheight" : 1259783 }
+func updateBtcInfo() error {
+	s, err := execCmd("pgrep", "-a", "bitcoind")
+	if err != nil {
+		return err
+	}
+	log.Fatalf("FIXMEH: bitcoind info: %q\n", s)
+	parts := strings.Split(s, " ")
+
+	btcInfo.pid = -1
+	btcInfo.args = parts[1:]
+	return nil
+}
 
 func main() {
+	if err := updateBtcInfo(); err != nil {
+		log.Fatalf("Failed to get bitcoind info: %v\n", err)
+	}
+
 	log.Printf("Running lightning-cli getinfo..\n")
 	c := cli{}
 	info, err := c.GetInfo()
 	if err != nil {
 		log.Fatalf("Failed to get info: %v\n", err)
 	}
+	log.Printf("lightningd info: %s\n", info)
 	// lightning-cli getinfo
 	// lightning-cli getpeers
 	// lightning-cli getnodes
