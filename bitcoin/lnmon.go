41019a5b5e2e32b5b4ce3908afc66681fea2e174
Henrik Jonsson <me@hkjn.me>
2018-01-24 17:22:38 +0000

    lnmon: Show getinfo output

--- bitcoin/lnmon.go
+++ bitcoin/lnmon.go
@@ -3,11 +3,18 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
+
+	"golang.org/x/crypto/acme/autocert"
 )
 
 type (
@@ -16,15 +23,42 @@ type (
 		pid  int
 		args []string
 	}
+	addressInfo struct {
+		AddressType string `json:"type"`
+		Address     string `json:"address"`
+		Port        int    `json:"port"`
+	}
+	// getinfo is the format of the getpeers response from lightning-cli.
+	getinfo struct {
+		NodeId      string        `json:"id"`
+		Port        int           `json:"port"`
+		Address     []addressInfo `json:"address"`
+		Version     string        `json:"version"`
+		Blockheight int           `json:"blockheight"`
+	}
+	// getpeers is the format of the getpeers response from lightning-cli.
+	getpeers struct {
+		NodeId      string        `json:"id"`
+		Port        int           `json:"port"`
+		Address     []addressInfo `json:"address"`
+		Version     string        `json:"version"`
+		Blockheight int           `json:"blockheight"`
+	}
 	lightningdInfo struct {
-		pid  int
-		args []string
+		pid     int
+		args    []string
+		getinfo getinfo
+	}
+	info struct {
+		bitcoind   bitcoindInfo
+		lightningd lightningdInfo
 	}
 )
 
 var (
-	btcInfo bitcoindInfo
-	lnInfo  lightningdInfo
+	state info
+	// btcInfo bitcoindInfo
+	// lnInfo  lightningdInfo
 )
 
 func (info bitcoindInfo) String() string {
@@ -64,8 +98,16 @@ func (c cli) exec(cmd string) (string, error) {
 	return execCmd("lightning-cli", cmd)
 }
 
-func (c cli) GetInfo() (string, error) {
-	return c.exec("getinfo")
+func (c cli) GetInfo() (*getinfo, error) {
+	infostring, err := c.exec("getinfo")
+	if err != nil {
+		return nil, err
+	}
+	var info getinfo
+	if err := json.Unmarshal([]byte(infostring), &info); err != nil {
+		return nil, err
+	}
+	return &info, nil
 }
 
 func (c cli) GetPeers() (string, error) {
@@ -76,75 +118,136 @@ func (c cli) GetNodes() (string, error) {
 	return "", nil
 }
 
-func updateBtcInfo() error {
+func getBtcInfo() (*bitcoindInfo, error) {
 	s, err := execCmd("pgrep", "-a", "bitcoind")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	parts := strings.Split(s, " ")
-	if len(parts) > 1 {
-		btcInfo.pid = -1
-		pid, err := strconv.Atoi(parts[0])
-		if err != nil {
-			return err
-		}
-		btcInfo.pid = pid
-
-		for _, arg := range parts[1:] {
-			btcInfo.args = append(btcInfo.args, arg)
-		}
-	} else {
-		btcInfo = bitcoindInfo{}
+	if len(parts) < 1 {
+		return &bitcoindInfo{}, nil
 	}
-	log.Println(btcInfo)
-	return nil
+	pid, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return nil, err
+	}
+	info := bitcoindInfo{
+		pid:  pid,
+		args: []string{},
+	}
+	for _, arg := range parts[1:] {
+		info.args = append(info.args, arg)
+	}
+	// log.Println(btcInfo)
+	return &info, nil
 }
 
-func updateLnInfo() error {
+func getLnInfo() (*lightningdInfo, error) {
 	s, err := execCmd("pgrep", "-a", "lightningd")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	parts := strings.Split(s, " ")
-	if len(parts) > 1 {
-		lnInfo.pid = -1
-		pid, err := strconv.Atoi(parts[0])
-		if err != nil {
-			return err
-		}
-		lnInfo.pid = pid
-
-		for _, arg := range parts[1:] {
-			lnInfo.args = append(lnInfo.args, arg)
-		}
-	} else {
-		lnInfo = lightningdInfo{}
+	if len(parts) < 1 {
+		return &lightningdInfo{}, nil
 	}
-	log.Println(lnInfo)
-	return nil
+	pid, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return nil, err
+	}
+	info := lightningdInfo{
+		pid:  pid,
+		args: []string{},
+	}
+	for _, arg := range parts[1:] {
+		info.args = append(info.args, arg)
+	}
+	c := cli{}
+	getinfo, err := c.GetInfo()
+	if err != nil {
+		return nil, err
+	}
+	info.getinfo = *getinfo
+	log.Printf("lightningd getinfo response: %+v\n", getinfo)
+	// log.Println(lnInfo)
+	return &info, nil
+}
+
+func refresh() {
+	for {
+		btcInfo, err := getBtcInfo()
+		if err != nil {
+			log.Fatalf("Failed to get bitcoind info: %v\n", err)
+		}
+		state.bitcoind = *btcInfo
+
+		lnInfo, err := getLnInfo()
+		if err != nil {
+			log.Fatalf("Failed to get lightningd info: %v\n", err)
+		}
+		state.lightningd = *lnInfo
+
+		// lightning-cli getinfo
+		// lightning-cli getpeers
+		// lightning-cli getnodes
+		// lightning-cli getchannels
+
+		// lightning-cli listfunds
+		// lightning-cli listinvoice
+		// lightning-cli listpayments
+		time.Sleep(time.Minute)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	s := "<html>"
+	s += "<h1>Hello!</h1>"
+	s += `<p>This is an experiment running at ln.hkjn.me with Lightning Network and Bitcoin by <a href="https://hkjn.me">hkjn</a>.</p>`
+	s += `<h2><code>bitcoind</code> info</h2>`
+	if state.bitcoind.pid == 0 {
+		s += `<p><code>bitcoind</code> is <strong>not running</strong>.</p>`
+	} else {
+		s += `<p><code>bitcoind</code> is <strong>running</strong>.</p>`
+	}
+	s += `<h2><code>lightningd</code> info</h2>`
+	if state.lightningd.pid == 0 {
+		s += `<p><code>lightningd</code> is <strong>not running</strong>.</p>`
+	} else {
+		s += `<p><code>lightningd</code> is <strong>running</strong>.</p>`
+		s += fmt.Sprintf(`<p>Our id is <code>%s</code></p>.`, state.lightningd.getinfo.NodeId)
+		s += fmt.Sprintf(`<p>Our address is is <code>%s:%d</code></p>.`, state.lightningd.getinfo.Address[0].Address, state.lightningd.getinfo.Address[0].Port)
+		s += fmt.Sprintf(`<p>Our version is is <code>%s:%d</code></p>.`, state.lightningd.getinfo.Version)
+		s += fmt.Sprintf(`<p>Our blockheight is is <code>%s:%d</code></p>.`, state.lightningd.getinfo.Blockheight)
+	}
+	s += "</html>"
+	fmt.Fprintf(w, s)
 }
 
 func main() {
-	if err := updateBtcInfo(); err != nil {
-		log.Fatalf("Failed to get bitcoind info: %v\n", err)
-	}
-	if err := updateLnInfo(); err != nil {
-		log.Fatalf("Failed to get lightningd info: %v\n", err)
-	}
+	go refresh()
 
-	log.Printf("Running lightning-cli getinfo..\n")
-	c := cli{}
-	info, err := c.GetInfo()
-	if err != nil {
-		log.Fatalf("Failed to get info: %v\n", err)
-	}
-	log.Printf("lightningd info: %s\n", info)
-	// lightning-cli getinfo
-	// lightning-cli getpeers
-	// lightning-cli getnodes
-	// lightning-cli getchannels
+	http.HandleFunc("/", indexHandler)
 
-	// lightning-cli listfunds
-	// lightning-cli listinvoice
-	// lightning-cli listpayments
+	addr := ":80"
+	if os.Getenv("LNMON_ADDR") != "" {
+		addr = os.Getenv("LNMON_ADDR")
+	}
+	hostname := "ln.hkjn.me"
+
+	fmt.Printf("Serving TLS at %q as %q..\n", addr, hostname)
+	s := &http.Server{
+		Addr: addr,
+	}
+	if addr == ":443" {
+		m := autocert.Manager{
+			Prompt:     autocert.AcceptTOS,
+			Cache:      autocert.DirCache("/etc/secrets/acme/"),
+			HostPolicy: autocert.HostWhitelist(hostname),
+		}
+		s.TLSConfig = &tls.Config{GetCertificate: m.GetCertificate}
+		log.Fatal(s.ListenAndServeTLS("", ""))
+	} else {
+		fmt.Printf("Serving plaintext HTTP on %s..\n", addr)
+		log.Fatal(s.ListenAndServe())
+	}
 }
