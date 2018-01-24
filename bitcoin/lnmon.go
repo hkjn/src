1b8050feffdba5049122ec87413a14eef9afb0fe
Henrik Jonsson <me@hkjn.me>
2018-01-24 15:54:08 +0000

    lnmon: Add bitcoind and lightningd info

--- bitcoin/lnmon.go
+++ bitcoin/lnmon.go
@@ -3,8 +3,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -14,15 +16,45 @@ type (
 		pid  int
 		args []string
 	}
+	lightningdInfo struct {
+		pid  int
+		args []string
+	}
 )
 
-var btcInfo bitcoindInfo
+var (
+	btcInfo bitcoindInfo
+	lnInfo  lightningdInfo
+)
 
+func (info bitcoindInfo) String() string {
+	if info.pid == 0 {
+		return "bitcoindInfo{not running}"
+	} else {
+		return fmt.Sprintf("bitcoindInfo{pid: %d, args: %q}", info.pid, strings.Join(info.args, " "))
+	}
+}
+
+func (info lightningdInfo) String() string {
+	if info.pid == 0 {
+		return "lightningdInfo{not running}"
+	} else {
+		return fmt.Sprintf("lightningdInfo{pid: %d, args: %q}", info.pid, strings.Join(info.args, " "))
+	}
+}
+
+// execCmd executes specified command with arguments and returns the output.
 func execCmd(cmd string, arg ...string) (string, error) {
 	c := exec.Command(cmd, arg...)
 	out := bytes.Buffer{}
+	stderr := bytes.Buffer{}
 	c.Stdout = &out
+	c.Stderr = &stderr
 	if err := c.Run(); err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			log.Printf("Command %q exited with non-zero status: %v, stderr=%s\n", cmd, err, stderr.String())
+			return stderr.String(), nil
+		}
 		return "", err
 	}
 	return out.String(), nil
@@ -49,17 +81,55 @@ func updateBtcInfo() error {
 	if err != nil {
 		return err
 	}
-	log.Fatalf("FIXMEH: bitcoind info: %q\n", s)
 	parts := strings.Split(s, " ")
+	if len(parts) > 1 {
+		btcInfo.pid = -1
+		pid, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return err
+		}
+		btcInfo.pid = pid
 
-	btcInfo.pid = -1
-	btcInfo.args = parts[1:]
+		for _, arg := range parts[1:] {
+			btcInfo.args = append(btcInfo.args, arg)
+		}
+	} else {
+		btcInfo = bitcoindInfo{}
+	}
+	log.Println(btcInfo)
+	return nil
+}
+
+func updateLnInfo() error {
+	s, err := execCmd("pgrep", "-a", "lightningd")
+	if err != nil {
+		return err
+	}
+	parts := strings.Split(s, " ")
+	if len(parts) > 1 {
+		lnInfo.pid = -1
+		pid, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return err
+		}
+		lnInfo.pid = pid
+
+		for _, arg := range parts[1:] {
+			lnInfo.args = append(lnInfo.args, arg)
+		}
+	} else {
+		lnInfo = lightningdInfo{}
+	}
+	log.Println(lnInfo)
 	return nil
 }
 
 func main() {
 	if err := updateBtcInfo(); err != nil {
 		log.Fatalf("Failed to get bitcoind info: %v\n", err)
+	}
+	if err := updateLnInfo(); err != nil {
+		log.Fatalf("Failed to get lightningd info: %v\n", err)
 	}
 
 	log.Printf("Running lightning-cli getinfo..\n")
