b18dc29ca483fff6318e637c0d05d8f339615083
Henrik Jonsson <me@hkjn.me>
2018-02-06 16:41:24 +0000

    caddy: Make sure GOPATH is defined

--- caddy/install.sh
+++ caddy/install.sh
@@ -1,7 +1,9 @@
 set -eu
 
-go get -v github.com/mholt/caddy/caddy
-go get -v github.com/caddyserver/builds
+GOPATH=${GOPATH:-""}
+
+go get -vu github.com/mholt/caddy/caddy
+go get -vu github.com/caddyserver/builds
 
 sudo setcap "cap_net_bind_service=+ep" ${GOPATH}/bin/caddy
 ulimit -n 8192 ${GOPATH}/bin/caddy
