54e0a85b94744e252368a50a0abb2309457607a8
Henrik Jonsson <me@hkjn.me>
2018-02-06 16:40:05 +0000

    caddy: Fix install.sh script

--- caddy/install.sh
+++ caddy/install.sh
@@ -1,7 +1,7 @@
-set -euo pipefail
+set -eu
 
 go get -v github.com/mholt/caddy/caddy
 go get -v github.com/caddyserver/builds
 
-sudo setcap "cap_net_bind_service=+ep" ~/bin/caddy
+sudo setcap "cap_net_bind_service=+ep" ${GOPATH}/bin/caddy
 ulimit -n 8192 ${GOPATH}/bin/caddy
