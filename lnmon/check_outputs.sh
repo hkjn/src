4bfd04e09325102cca8a058b3da0197fc3dad7b2
Henrik Jonsson <me@hkjn.me>
2018-02-22 15:46:46 +0000

    lnmon: Add more info to check_outputs.sh

--- lnmon/check_outputs.sh
+++ lnmon/check_outputs.sh
@@ -5,9 +5,9 @@ echo "Listing all known outputs:"
 echo "----------------------------"
 sqlite3 .lightning/lightningd.sqlite3 <<EOF
 .headers on
-.mode tabs
+.mode csv
 SELECT
-	HEX(prev_out_tx),
+	HEX(prev_out_tx) AS prev_out_tx_hex,
 	prev_out_index,
 	value,
 	-- Via wallet_output_type defined at https://github.com/ElementsProject/lightning/blob/master/wallet/wallet.h#L47
@@ -33,13 +33,40 @@ FROM outputs;
 EOF
 
 echo "----------------------------"
-echo "Summing up spendable UTXOs.."
+echo "Listing available outputs:"
 echo "----------------------------"
 sqlite3 .lightning/lightningd.sqlite3 <<EOF
 .headers on
-.mode tabs
+.mode csv
 SELECT
-	SUM(value),
+	HEX(prev_out_tx) AS prev_out_tx_hex,
+	prev_out_index,
+	value,
+	-- Via wallet_output_type defined at https://github.com/ElementsProject/lightning/blob/master/wallet/wallet.h#L47
+	CASE type
+		WHEN '0' THEN 'p2sh_wpkh'
+		WHEN '1' THEN 'to_local'
+		WHEN '3' THEN 'htlc_offer'
+		WHEN '4' THEN 'htlc_offer'
+		WHEN '4' THEN 'htlc_recv'
+		WHEN '5' THEN 'our_change'
+		WHEN '6' THEN 'p2wpkh'
+		ELSE 'unknown_state'
+	END AS type_desc
+FROM outputs
+WHERE
+  -- output_status_available
+  status=0;
+EOF
+
+echo "----------------------------"
+echo "Summing up spendable UTXOs.."
+echo "----------------------------"
+RESULT=$(sqlite3 .lightning/lightningd.sqlite3 <<EOF
+.headers on
+.mode csv
+SELECT
+	SUM(value) AS sum,
 	-- Via wallet_output_type defined at https://github.com/ElementsProject/lightning/blob/master/wallet/wallet.h#L47
 	CASE type
 		WHEN '0' THEN 'p2sh_wpkh'
@@ -57,3 +84,10 @@ WHERE
   status=0
 GROUP BY type_desc;
 EOF
+)
+echo "${RESULT}"
+# TODO: could reverse to get little-endian version of txids, i.e. compatible with bitcoin-cli:
+# d='${RESULT}'
+# txids=[x.split(',')[0] for x in d.split()[1:]]
+# for txid in txids:
+#   print(''.join(txid[i:i+2] for i in range(0, len(txid), 2)[::-1]))
