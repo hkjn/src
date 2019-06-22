ff50694bacf94945a0f94fdf5881a58ea9557830
Henrik Jonsson <me@hkjn.me>
2019-06-22 15:20:03 +0200

    scripts: Add mount to host for tor browser

--- scripts/tor_browser.sh
+++ scripts/tor_browser.sh
@@ -6,5 +6,6 @@ sudo docker run -it --rm \
   --memory 3G \
   --name tor-browser \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v ${HOME}/tor-downloads:/usr/local/bin/Browser/Downloads \
   -e DISPLAY=unix${DISPLAY} \
   hkjn/tor-browser:9.0a2
