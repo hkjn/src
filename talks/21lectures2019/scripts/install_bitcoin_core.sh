7865f3b6b6d48eeb79b3d3452766319035242b84
Henrik Jonsson <me@hkjn.me>
2019-06-27 06:34:48 +0200

    talks: Make install script idempotent

--- talks/21lectures2019/scripts/install_bitcoin_core.sh
+++ talks/21lectures2019/scripts/install_bitcoin_core.sh
@@ -11,18 +11,25 @@ command -v gpg >/dev/null 2>&1 || { echo >&2 "gpg is missing"; exit 1; }
 command -v wget >/dev/null 2>&1 || { echo >&2 "mkdir is missing"; exit 1; }
 command -v tar >/dev/null 2>&1 || { echo >&2 "cat is missing"; exit 1; }
 command -v mkdir >/dev/null 2>&1 || { echo >&2 "chmod is missing"; exit 1; }
-cd
+cd "${HOME}"
 
-echo "*********************************************"
-echo "Fetching Bitcoin Core and the signed hashes.."
-wget https://bitcoincore.org/bin/bitcoin-core-0.18.0/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
-wget https://bitcoincore.org/bin/bitcoin-core-0.18.0/SHA256SUMS.asc
+[[ -e bitcoin-0.18.0-x86_64-linux-gnu.tar.gz ]] || {
+    echo "*********************************************"
+    echo "Fetching Bitcoin Core and the signed hashes.."
+    wget https://bitcoincore.org/bin/bitcoin-core-0.18.0/bitcoin-0.18.0-x86_64-linux-gnu.tar.gz
+    wget https://bitcoincore.org/bin/bitcoin-core-0.18.0/SHA256SUMS.asc
+}
+
 # if gpg servers are unstable, use local copy of key:
 # wget https://j.mp/btc-release-key
 # gpg --import < 01EA5486DE18A882D4C2684590C8019E36C2E964.asc
 echo "*********************************************"
-echo "Verifying GPG signature.."
+echo "Fetching release key.."
 gpg --recv-key 01EA5486DE18A882D4C2684590C8019E36C2E964
+echo "*********************************************"
+
+echo "*********************************************"
+echo "Verifying GPG signature.."
 gpg --verify SHA256SUMS.asc
 echo "*********************************************"
 
