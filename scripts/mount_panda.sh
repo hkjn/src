0d994beb1be5e0f7120ba4b10b4482d9fa2cb95c
Henrik Jonsson <me@hkjn.me>
2019-07-07 15:25:54 +0200

    scripts: Also mount/umount panda_son

--- scripts/mount_panda.sh
+++ scripts/mount_panda.sh
@@ -9,3 +9,6 @@ set -euo pipefail
 sudo mkdir -p /media/panda
 sudo mount /dev/mapper/panda /media/panda/
 echo "Mounted /media/panda."
+
+sudo mount /dev/disk/by-id/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0:0-part6 /media/panda_son
+echo "Mounted /media/panda_son."
--- scripts/umount_panda.sh
+++ scripts/umount_panda.sh
@@ -8,4 +8,9 @@ echo "Running fsck.."
 sudo fsck /dev/mapper/panda
 sudo cryptsetup remove panda
 
+sudo umount /media/panda_son
+echo "Unmounted /media/panda_son."
+echo "Running fsck.."
+sudo fsck /dev/disk/by-id/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0:0-part6
+
 echo "All done."
