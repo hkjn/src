c78236c7b3d491f028359a455690436194c978ab
Henrik Jonsson <me@hkjn.me>
2019-07-26 07:43:07 +0000

    nixos: Add electrum, htop packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -65,12 +65,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+     electrum
      firefox
      git
      gnupg
      gnumake
      go
      file
+     htop
      mosh
      mkpasswd
      mplayer
