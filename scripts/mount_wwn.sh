a9d7a53151596d35413b6711b8f5314d6dccafe9
Henrik Jonsson <me@hkjn.me>
2019-07-29 16:28:38 +0000

    scripts: Also mount/unmount exfat in wwn

--- scripts/mount_wwn.sh
+++ scripts/mount_wwn.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
+sudo mkdir -p /media/wwn /media/wwn_exfat
 [[ -e /dev/mapper/wwn-unlocked ]] || {
   sudo cryptsetup luksOpen --key-file=/root/keys/ata-SanDisk_SD9SN8W1T00_183255420707-part3.key /dev/disk/by-id/ata-SanDisk_SD9SN8W1T00_183255420707-part3 wwn-unlocked
 }
 
+sudo mount /dev/disk/by-id/ata-SanDisk_SD9SN8W1T00_183255420707-part1 /media/wwn_exfat
 sudo mount /dev/mapper/wwn-unlocked /media/wwn/
 echo "Mounted /media/wwn."
--- scripts/umount_wwn.sh
+++ scripts/umount_wwn.sh
@@ -8,4 +8,9 @@ echo "Running fsck.."
 sudo fsck /dev/mapper/wwn-unlocked
 sudo cryptsetup remove wwn-unlocked
 
+sudo umount /media/wwn_exfat
+echo "Unmounted /media/wwn_exfat."
+echo "Running fsck.."
+sudo fsck /dev/disk/by-id/ata-SanDisk_SD9SN8W1T00_183255420707-part1
+
 echo "All done."
