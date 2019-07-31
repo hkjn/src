b2dc2e745f14765fc7beb804a20299eacdd8207c
Henrik Jonsson <me@hkjn.me>
2019-07-31 07:17:52 +0000

    scripts: Make mount_panda.sh more robust

--- scripts/mount_panda.sh
+++ scripts/mount_panda.sh
@@ -3,12 +3,13 @@
 set -euo pipefail
 
 [[ -e /dev/mapper/panda ]] || {
-	sudo cryptsetup luksOpen --key-file=/root/keys/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0\:0-part1.key /dev/disk/by-id/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0\:0-part1 panda
+	sudo cryptsetup luksOpen --key-file=/root/keys/usb-WD_ELEMENTS_SE_25FF_etc-part1.key /dev/disk/by-id/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0\:0-part1 panda
 }
 
 sudo mkdir -p /media/panda
 sudo mount /dev/mapper/panda /media/panda/
 echo "Mounted /media/panda."
 
+sudo mkdir -p /media/panda_son
 sudo mount /dev/disk/by-id/usb-WD_Elements_SE_25FF_5758333144343835535A304A-0:0-part6 /media/panda_son
 echo "Mounted /media/panda_son."
