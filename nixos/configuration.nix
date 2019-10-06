79b489a2dd7b80deb76746918621a6104a38416a
Henrik Jonsson <me@hkjn.me>
2019-10-06 11:34:34 -0700

    nixos: Add arandr

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,6 +76,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+     arandr
      bind
      electrum
      exfat
