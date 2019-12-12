27b2dd6c98f4cedc1ebadd8df07713b951149fec
Henrik Jonsson <me@hkjn.me>
2019-12-11 19:31:47 -0800

    infra/scaleway: Remove backends, bump image

--- infra/scaleway/backends.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket                  = "zterraform-state"
-    key                     = "hkjn.me/src/infra/scaleway/terraform.tfstate"
-    region                  = "eu-west-1"
-    shared_credentials_file = ".backend_credentials"
-  }
-}
-
--- infra/scaleway/variables.tf
+++ infra/scaleway/variables.tf
@@ -3,6 +3,6 @@ variable "image" {
   # curl -H "X-Auth-Token: $(cat .scaleway/scaleway0_token)" -H 'Content-Type: application/json' 'https://cp-par1.scaleway.com/images/?page=1&per_page=100' > scaleway_images.json
   # >>> import json; d=json.loads(open('scaleway_images.json').read())
   # >>> pprint.pprint([x for x in d['images'] if x['arch'] == 'arm' and 'Ubuntu Xenial' in x['name']])
-  default = "f71f2ad9-7810-405b-9181-2e8d5e1feb18"
+  default = "8c76c2b9-926e-44c5-b250-6f6480b5d313"
 }
 
